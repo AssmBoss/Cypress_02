bae9e522bb2cef2a6199daafc5d09bf1016ca511
AssmBoss <assmboss@gmail.com>
2023-02-04 11:53:26 +0300

    Parallel run is done.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "83nqm8",
     baseUrl: 'http://qamid.tmweb.ru/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
